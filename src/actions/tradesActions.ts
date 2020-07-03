a85f8fb9828b31db4fb48ab868e602fd220aa42f
sergey_sss <sergeylabsss@gmail.com>
2020-07-03 10:42:36 +0300

    Paxful test demo

---
+++ src/actions/tradesActions.ts
@@ -0,0 +1,81 @@
+import { Trade } from 'entries/trade';
+import {
+  GET_TRADES_REQUEST,
+  GET_TRADES_SUCCESS,
+  GET_TRADES_ERROR,
+  DELETE_TRADES,
+  CHANGE_TRADE_STATUS,
+} from '../constants/actionTypes';
+import delayForResponse from '../helper/delayForResponse';
+
+export type Args = {
+  tradeId: string,
+  message?: {
+    id: number,
+    text: string,
+    type: string,
+    date: string,
+  },
+  account?: string,
+  prevId?: string,
+};
+
+export type Results = {
+  type: string,
+  payload: Args,
+};
+
+type GetTradesRequestType = () => { type: string };
+type GetTradesSuccessType = (
+  payload: Array<Trade>,
+) => {
+  type: string,
+  payload: Array<Trade>,
+};
+type GetTradesErrorType = (
+  payload: string,
+) => {
+  type: string,
+  payload: string,
+};
+type TypeOnlyWithId = (payload: string) => Results;
+
+const getTradesRequest: GetTradesRequestType = () => ({
+  type: GET_TRADES_REQUEST,
+});
+
+const getTradesSuccess: GetTradesSuccessType = (payload) => ({
+  type: GET_TRADES_SUCCESS,
+  payload,
+});
+
+const getTradesError: GetTradesErrorType = (payload) => ({
+  type: GET_TRADES_ERROR,
+  payload,
+});
+
+export const deleteTrade: TypeOnlyWithId = (payload) => ({
+  type: DELETE_TRADES,
+  payload: { tradeId: payload },
+});
+
+export const changeTradeStatus: TypeOnlyWithId = (payload) => ({
+  type: CHANGE_TRADE_STATUS,
+  payload: { tradeId: payload },
+});
+
+export const getTrades = (url: string) => async (dispatch: any) => {
+  dispatch(getTradesRequest());
+
+  await delayForResponse();
+
+  const response = await fetch(url);
+
+  if (!response.ok) return dispatch(getTradesError(response.statusText));
+
+  const result: Array<Trade> = await response.json();
+
+  return dispatch(getTradesSuccess(result));
+};
+
+export default getTrades;
---
+++ src/components/blocks/DialogDetails/component.tsx
@@ -0,0 +1,93 @@
+import React, { FC, useCallback } from 'react';
+import Moment from 'react-moment';
+import 'moment-timezone';
+import { useSelector, useDispatch } from 'react-redux';
+import { SELLER_TYPE } from 'constants/account';
+import ChangeUserButton from 'components/blocks/ChangeUserButton';
+import Button from 'components/controls/Button';
+import { changeTradeStatus } from 'actions/tradesActions';
+import FromServer from 'components/blocks/FromServer';
+import { Store } from 'entries/store';
+import Rating from '../Rating';
+
+import './style.css';
+import UserAvatar from '../UserAvatar';
+
+const DialogDetails: FC = () => {
+  const account = useSelector((state: Store) => state.account);
+  const currentTrade = useSelector((state: Store) => state.currentTrade.item);
+  const dispatch = useDispatch();
+
+  const {
+    name,
+    rating,
+    avatar,
+    isPaid,
+    numberOfTrades,
+    tradeHash,
+    startAt,
+  } = currentTrade;
+
+  const handleOnClick = useCallback(() => {
+    if (!isPaid) dispatch(changeTradeStatus(currentTrade.id));
+  }, [currentTrade.id]);
+
+  return (
+    <>
+      <div className="trades-dialogs__details-hearer">
+        You are trading with <span>{name}</span>
+        <div className="trades-dialogs__start-time">
+          Started{' '}
+          <Moment tz="Russia/Moscow" fromNow interval={10000}>
+            {startAt}
+          </Moment>
+        </div>
+        {account === SELLER_TYPE && !isPaid && (
+          <Button
+            text="Release bitcoins"
+            customClass="grin-btn medium"
+            onClick={handleOnClick}
+            htmlType="button"
+          />
+        )}
+        {account === SELLER_TYPE && isPaid && 'You are released bitcoins!'}
+      </div>
+      <ul className="trades-dialogs__details-body">
+        <li>
+          <UserAvatar imageLink={avatar} />
+          <Rating rating={rating} />
+        </li>
+        <li>
+          <div className="trades-dialogs__details-title"># of trades</div>
+          <div className="trades-dialogs__details-text">{numberOfTrades}</div>
+        </li>
+        <li>
+          <div className="trades-dialogs__details-title">trade status</div>
+          <span className={`trade-status ${isPaid ? 'paid' : null}`}>
+            {isPaid ? 'paid' : 'not paid'}
+          </span>
+        </li>
+        <li>
+          <div className="trades-dialogs__details-title">trade hash</div>
+          <div className="trades-dialogs__details-text">{tradeHash}</div>
+        </li>
+        <li>
+          <div className="trades-dialogs__details-title">amount USD</div>
+          <div className="trades-dialogs__details-text cost">25.00</div>
+        </li>
+        <li>
+          <div className="trades-dialogs__details-title">amount BTC</div>
+          <div className="trades-dialogs__details-text">0.00234524</div>
+        </li>
+      </ul>
+      <div className="information-from-server">
+        <FromServer />
+      </div>
+      <div className="trades-dialogs__change-user">
+        <ChangeUserButton />
+      </div>
+    </>
+  );
+};
+
+export default DialogDetails;
